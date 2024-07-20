de05cae7edc9e1c05b35158130021f80620eb87d
Bikash74 <shaw46482@gmail.com>
2024-07-21 00:51:23 +0530

    connected to mongodb

---
+++ src/main/java/org/example/quorabackendspring/QuoraBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.quorabackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraBackendSpringApplication.class, args);
+    }
+
+}
